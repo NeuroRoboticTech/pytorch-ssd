53f5e5587d4efbec2a5e37b4711980fa64e8fb42
David Cofer <davidcofer73@gmail.com>
2023-04-08 13:40:57 -0500

    added script to blank part of an image.

--- utils/blank_image_portion.py
+++ utils/blank_image_portion.py
@@ -4,6 +4,9 @@ import shutil
 import sys
 import logging
 
+import cv2
+
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
@@ -19,24 +22,23 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
 
-partition_group = 14
+partition_group = 13
 
 img_dir = '/media/dcofer/Ubuntu_Data/active_shooter_defense/gun_dataset/detector/other/train/images/'
-labels_dir = '/media/dcofer/Ubuntu_Data/active_shooter_defense/gun_dataset/detector/other/train/labels_combined/'
-
-# output_img_dir = '/media/dcofer/3922-2939/group_' + str(partition_group + 1) + '/images/'
-# output_label_dir = '/media/dcofer/3922-2939/group_' + str(partition_group + 1) + '/labels/'
 
 output_img_dir = '/media/dcofer/Ubuntu_Data/active_shooter_defense/gun_dataset/detector/other/train/group_' + \
                  str(partition_group + 1) + '/images/'
-output_label_dir = '/media/dcofer/Ubuntu_Data/active_shooter_defense/gun_dataset/detector/other/train/group_' + \
-                   str(partition_group + 1) + '/labels/'
+orig_img_dir = '/media/dcofer/Ubuntu_Data/active_shooter_defense/gun_dataset/detector/other/train/group_' + \
+                   str(partition_group + 1) + '/images_orig/'
+
+if not os.path.exists(img_dir):
+    raise RuntimeError("images dir not found.")
 
 if not os.path.exists(output_img_dir):
-    os.makedirs(output_img_dir)
+    raise RuntimeError("original images not found.")
 
-if not os.path.exists(output_label_dir):
-    os.makedirs(output_label_dir)
+if not os.path.exists(orig_img_dir):
+    os.makedirs(orig_img_dir)
 
 files = glob.glob(os.path.join(img_dir, '*.jpg'))
 files.sort()
@@ -47,24 +49,37 @@ start_idx = group_size * partition_group
 end_idx = group_size * partition_group + group_size
 group_files = files[start_idx:end_idx]
 total_files = len(group_files)
+
 done = False
-idx = 1
-for img_file in group_files:
+idx = 0
+start_img_idx = 0
+end_img_idx = 1230  # 4979
+top_left = (156, 400)
+bottom_right = (440, 497)
+# for idx in range(0, total_files):
+#     img_file = group_files[idx]
+#     basename = os.path.basename(img_file)[:-4]
+#     logging.info("idx: %d, file: %s", idx, basename)
+
+for idx in range(start_img_idx, end_img_idx):
+    img_file = group_files[idx]
     basename = os.path.basename(img_file)[:-4]
     logging.info("processing %d: %s", idx, basename)
 
-    label_file = labels_dir + basename + ".txt"
+    save_img_file = output_img_dir + basename + ".jpg"
+    copy_img_file = orig_img_dir + basename + ".jpg"
 
-    copy_img_file = output_img_dir + basename + ".jpg"
-    copy_label_file = output_label_dir + basename + ".txt"
-
+    # Copy the original image file to orig_img files
     img_file_exists = os.path.exists(img_file)
-    label_file_exists = os.path.exists(label_file)
-    if img_file_exists and label_file_exists:
-        shutil.copy(img_file, copy_img_file)
-        shutil.copy(label_file, copy_label_file)
-    else:
-        logging.info("file: %s is missing label of image.", basename)
-    idx += 1
+    shutil.copy(img_file, copy_img_file)
+
+    # Now fix the original image.
+    img = cv2.imread(img_file)
+    img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]] = (0, 0, 0)
+
+    # cv2.imshow("Updated", img)
+    # cv2.waitKey(0)
+
+    cv2.imwrite(save_img_file, img)
 
 logging.info("finished processing.")
